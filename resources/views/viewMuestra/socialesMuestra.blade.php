ebfb2d3a316e17aa34586321b40f9dc543c11258
grisvelazquez <grisvelazquez.v@gmail.com>
2016-05-26 15:36:49 -0500

    agregar menú de redes sociales a página de Muestra Iberoamericana

---
+++ resources/views/viewMuestra/socialesMuestra.blade.php
@@ -0,0 +1,29 @@
+<div class="container">
+            <div class="row mobile-social-share">
+                <div class="col-md-9"></div>
+                <div id="socialHolder" class="col-md-3">
+                    <div id="socialShare" class="btn-group share-group sin-borde">
+                        <a data-toggle="dropdown" class="btn btn-info">
+                            <i class="fa fa-share-alt fa-inverse fa-2x"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a data-original-title="Twitter" rel="tooltip"  href="#" class="btn btn-twitter over-logo">
+                                    <i class="fa fa-twitter fa-2x"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a data-original-title="Facebook" rel="tooltip"  href="#" class="btn btn-facebook">
+                                    <i class="fa fa-facebook fa-2x"></i>
+                                </a>
+                            </li>					
+<!--                            <li>
+                                <a data-original-title="Youtube" rel="tooltip"  href="#" class="btn btn-youtube">
+                                    <i class="fa fa-youtube-play fa-2x"></i>
+                                </a>
+                            </li>-->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
