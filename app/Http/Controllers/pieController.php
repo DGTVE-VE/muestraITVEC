66360df4b4511a23c3cffd09675b58b75f64009c
EDA1 <EDA1>
2016-06-02 17:36:37 -0500

    Pruebas de envio de correo contacto

--- app/Http/Controllers/pieController.php
+++ app/Http/Controllers/pieController.php
@@ -19,18 +19,18 @@ class pieController extends Controller {
 
 	public function guardaContacto(){
 
-		/*$guardaDatos = DB::table('muestra_contacto')->insert(['nombre_contacto'=>$_POST['nombre_contacto'],
-						'correo'=>$_POST['correo'],'asunto'=>$_POST['asunto'],'mensaje'=>$_POST['mensaje']]);*/
-		$datosContacto = [$_POST['nombre_contacto'],$_POST['correo'],$_POST['asunto']];
-		$guardaDatos = "1";
+		$guardaDatos = DB::table('muestra_contacto')->insert(['nombre_contacto'=>$_POST['nombre_contacto'],
+						'correo'=>$_POST['correo'],'asunto'=>$_POST['asunto'],'mensaje'=>$_POST['mensaje']]);
+		/*$datosContacto = [$_POST['nombre_contacto'],$_POST['correo'],$_POST['asunto']];
+		$guardaDatos = "1";*/
 		
-		$correo = 'mitec@televisioneducativa.gob.mx';
-		//$hash = md5(date('Y/m/d H:i:s'));
-		$hash = "f15da72cb94dd31add8e1daa6d65b68e";
+		/*$correo = 'mitec@televisioneducativa.gob.mx';
+		$hash = md5(date('Y/m/d H:i:s'));
+		//$hash = "f15da72cb94dd31add8e1daa6d65b68e";
 		Mail::send('viewMuestra.mailActivacion', ['correo' => $correo, 'hash' => $hash, 'datosContacto' => $datosContacto], function ($m) use ($correo) {
             $m->from('mitec@televisioneducativa.gob.mx', 'Muestra Iberoamericana');
             $m->to($correo)->subject('Comentarios Recibidos');
-        });
+        });*/
 		return view('viewMuestra.contacto')->with('guardaDatos',$guardaDatos);
 	}
 }
