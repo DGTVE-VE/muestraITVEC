dc0104d646d048829b503882344c710783bc5764
Luis Sassón <luisasson2911@gmail.com>
2016-06-15 11:00:52 -0500

    facebook integration 1

--- resources/views/viewMuestra/pieMuestra.blade.php
+++ resources/views/viewMuestra/pieMuestra.blade.php
@@ -1,4 +1,25 @@
 <div class="row" style="background-color: #818181;">
+    <!--script para el api de Facebook-->
+    <script>
+        window.fbAsyncInit = function () {
+            FB.init({
+                appId: '606978862799823',
+                xfbml: true,
+                version: 'v2.6'
+            });
+        };
+
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
     <div class="col-md-3" >
         <center>
             <br><br><br>
@@ -11,7 +32,12 @@
                 <a data-original-title="Facebook" rel="tooltip"  href="#" class="btn btn-facebook">
                     <i class="fa fa-facebook fa-2x"></i>
                 </a>
-
+                <div
+                    class="fb-like"
+                    data-share="true"
+                    data-width="450"
+                    data-show-faces="true">
+                </div>
             </div>
         </center>
     </div>
@@ -53,7 +79,17 @@
         <h3 class="text-center textBlanco titulofases">CEDE DEL EVENTO</h3>
         <hr>    
         <center>             
-            <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:300px;width:500px;'><div id='gmap_canvas' style='height:300px;width:500px;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="https://termsofusegenerator.net">terms of use generator</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map(){var myOptions = {zoom:16, center:new google.maps.LatLng(19.35699452613836, - 99.1407719619018), mapTypeId: google.maps.MapTypeId.ROADMAP}; map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions); marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(19.35699452613836, - 99.1407719619018)}); infowindow = new google.maps.InfoWindow({content:'<strong>Centro Nacional de las artes</strong><br>Avenida Río Churubusco 79, Coyoacán, Colonia Country Club, 04220 Ciudad de México<br>'}); google.maps.event.addListener(marker, 'click', function(){infowindow.open(map, marker); }); infowindow.open(map, marker); }google.maps.event.addDomListener(window, 'load', init_map);</script>
+            <script src='https://maps.googleapis.com/maps/api/js?v=3.exp'></script><div style='overflow:hidden;height:300px;width:500px;'><div id='gmap_canvas' style='height:300px;width:500px;'></div><div><small><a href="http://embedgooglemaps.com">									embed google maps							</a></small></div><div><small><a href="https://termsofusegenerator.net">terms of use generator</a></small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></div><script type='text/javascript'>function init_map() {
+            var myOptions = {zoom: 16, center: new google.maps.LatLng(19.35699452613836, -99.1407719619018), mapTypeId: google.maps.MapTypeId.ROADMAP};
+            map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+            marker = new google.maps.Marker({map: map, position: new google.maps.LatLng(19.35699452613836, -99.1407719619018)});
+            infowindow = new google.maps.InfoWindow({content: '<strong>Centro Nacional de las artes</strong><br>Avenida Río Churubusco 79, Coyoacán, Colonia Country Club, 04220 Ciudad de México<br>'});
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+            infowindow.open(map, marker);
+        }
+        google.maps.event.addDomListener(window, 'load', init_map);</script>
             <br>
         </center>
     </div>
